f47406155fa4b5e533a26d78d48aa249d903fc6b
Jase <tin.nguyen@rakuten.com>
2023-02-03 10:11:07 +0900

    Clean up commented code

--- controllers/record_controller.go
+++ controllers/record_controller.go
@@ -48,21 +48,3 @@ func CreateRecord(c *gin.Context) {
 	conf.DB.Create(record)
 	c.JSON(http.StatusCreated, gin.H{"data": record})
 }
-
-/*
-func validateNewRecordPayload(p models.CreateRecordInput) error {
-	// Check if the Datetime field is in the correct format.
-	datetimeRegexp := regexp.MustCompile(`\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z`)
-	if !datetimeRegexp.MatchString(p.DateTime) {
-		return fmt.Errorf("The Datetime field is not in the correct format: %s", p.DateTime)
-	}
-
-	// Try to parse the Datetime field as a time.Time.
-	_, err := time.Parse(time.RFC3339, p.DateTime)
-	if err != nil {
-		return fmt.Errorf("The Datetime field is not a valid time: %v", err)
-	}
-
-	return nil
-}
-*/
